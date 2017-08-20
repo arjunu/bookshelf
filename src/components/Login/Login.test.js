6ea31fd296ac1e123997f2b6efc7d49dc98228f1
Arjun U <ar5junu@gmail.com>
2017-08-21 00:34:34 +0530

    Install cross-env, update coverage

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -29,6 +29,16 @@ describe('Login component tests', () => {
         expect(login.state().username).toEqual('a');
     });
 
+    test('On input change of password, the entered text is saved in state', ()=> {
+        const login = shallow(
+            <Login onLogin={f => f}/>
+        );
+
+        expect(login.state().password).toEqual('');
+        login.find("input").at(1).simulate('change', {target: {value: "a"}});
+        expect(login.state().password).toEqual('a');
+    });
+
     test('OnLogin is called with arguments when Login button is clicked', () => {
         const onLogin = jest.fn();
 
